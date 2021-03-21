f603be160b446186d9c4a10277d6432b8cfbf139
trevor-shepard <trevorbshepard@gmail.com>
2021-03-21 10:43:59 -0500

    add firebase util

---
+++ src/utils/firebase.js
@@ -0,0 +1,45 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import 'firebase/functions'
+
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(config)
+}
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+export const storage = firebase.storage()
+export const functions = firebase.functions()
+export default firebase
+
+export const handleFireBaseImageUpload = async (path, photo) => {
+	console.log('start of upload')
+	if (photo === null)
+		throw Error(`not an image, the image file is a ${typeof photo}`)
+	const downloadURL = await storage
+		.ref(path)
+		.put(photo)
+		.then(async () => {
+			// gets the functions from storage refences the image storage in firebase by the children
+			// gets the download url then sets the image from firebase as the value for the imgUrl key:
+			return await storage.ref(path).getDownloadURL()
+		})
+		.catch((error) => {
+			const message = error.message
+			console.log(message)
+		})
+
+	return downloadURL
+}
